3450506ac56b11bbf147fa100a8fe194d6a93db1
Sendula Márk <marksendula@gmail.com>
2024-11-19 16:55:39 +0100

    Books-NestJS

--- src/create-sutemeny.dto.ts
+++ src/create-sutemeny.dto.ts
@@ -1,5 +1,16 @@
+import { IsBoolean, IsInt, IsNotEmpty, IsString, IsIn } from "class-validator";
+
 export class CreateSutemenyDto {
+    @IsString()
+    @IsNotEmpty({ message: 'A nevet kötelező megadni!'})
     name: string;
+
+    @IsBoolean()
     laktozMentes: boolean;
+
+    @IsInt()
     db: number;
+
+    // @IsIn(['torta', 'fagylalt', 'édesség'])
+    // tipus: string;
 }
